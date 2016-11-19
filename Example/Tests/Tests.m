cb5bf1307f327dbad177bd37b193b5d6f8d6c98e
dbukowski <dariusz.m.bukowski@gmail.com>
2016-11-19 16:16:48 +0100

    Removed example failing tests.

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -10,23 +10,6 @@
 
 SpecBegin(InitialSpecs)
 
-describe(@"these will fail", ^{
-
-    it(@"can do maths", ^{
-        expect(1).to.equal(2);
-    });
-
-    it(@"can read", ^{
-        expect(@"number").to.equal(@"string");
-    });
-    
-    it(@"will wait for 10 seconds and fail", ^{
-        waitUntil(^(DoneCallback done) {
-        
-        });
-    });
-});
-
 describe(@"these will pass", ^{
     
     it(@"can do maths", ^{
