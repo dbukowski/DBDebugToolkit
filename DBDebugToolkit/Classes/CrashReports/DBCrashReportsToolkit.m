ed4df116ab9382ef4b5a770054ce25f9c67c1d8a
dbukowski <dariusz.m.bukowski@gmail.com>
2017-10-06 19:46:27 +0200

    Added debugger warning.

--- DBDebugToolkit/Classes/CrashReports/DBCrashReportsToolkit.m
+++ DBDebugToolkit/Classes/CrashReports/DBCrashReportsToolkit.m
@@ -22,6 +22,11 @@
 
 #import "DBCrashReportsToolkit.h"
 #import "UIView+Snapshot.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/sysctl.h>
 
 typedef void (*sighandler_t)(int);
 
@@ -96,6 +101,10 @@ static sighandler_t _previousSIGPIPEHandler;
 }
 
 - (void)setupCrashReporting {
+    if ([self isRunUnderDebugger]) {
+        NSLog(@"[DBDebugToolkit] The app is running under the debugger. It may not generate crash reports correctly.");
+    }
+
     self.isCrashReportingEnabled = YES;
 
     // Uncaught exceptions
@@ -227,6 +236,42 @@ static void handleSIGPIPESignal(int sig) {
                                                attributes:nil
                                                     error:nil];
     [dictionaryRepresentation writeToFile:filePath atomically:YES];
+}
+
+// Code from iOS Technical Q&A: https://developer.apple.com/library/content/qa/qa1361/_index.html
+- (BOOL)isRunUnderDebugger {
+#ifndef DEBUG
+    return NO;
+#endif
+    // Returns true if the current process is being debugged (either
+    // running under the debugger or has a debugger attached post facto).
+
+    int                 junk;
+    int                 mib[4];
+    struct kinfo_proc   info;
+    size_t              size;
+
+    // Initialize the flags so that, if sysctl fails for some bizarre
+    // reason, we get a predictable result.
+
+    info.kp_proc.p_flag = 0;
+
+    // Initialize mib, which tells sysctl the info we want, in this case
+    // we're looking for information about a specific process ID.
+
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_PROC;
+    mib[2] = KERN_PROC_PID;
+    mib[3] = getpid();
+
+    // Call sysctl.
+
+    size = sizeof(info);
+    junk = sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, NULL, 0);
+    assert(junk == 0);
+
+    // We're being debugged if the P_TRACED flag is set.
+    return ( (info.kp_proc.p_flag & P_TRACED) != 0 );
 }
 
 @end
